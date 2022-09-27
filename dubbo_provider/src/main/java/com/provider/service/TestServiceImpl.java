44f8f2c273543522e4ff40a79a4c5c2c71e7ad67
admin <huilong.yang@42pay.com>
2022-09-27 18:53:48 +0800

    调整pom

--- dubbo_provider/src/main/java/com/provider/service/TestServiceImpl.java
+++ dubbo_provider/src/main/java/com/provider/service/TestServiceImpl.java
@@ -3,16 +3,12 @@ import com.api.Result;
 import com.api.TestService;
 import org.apache.dubbo.config.annotation.Service;
 
-/**
- * @ClassName TestServiceImpl
- * Description
- * @Author admin
- * @Date 2022/9/22 12:31 PM
- **/
+
 @Service
 public class TestServiceImpl implements TestService {
     @Override
     public Result hello(String name) {
+        System.out.println(this.getClass());
         return  Result.fail("1","suc",name);
     }
 }
